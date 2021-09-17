b8f946bbd38b1dd0f48e96b7b9e2eaee04d59d25
feliciaabelsson <you@example.comf.abelsson@live.se>
2021-09-17 16:46:30 +0200

    Web server with GET /

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-//first run environmental variable, if not run port 1337
+//If heroku gives us a PORT the the environmental variable will run, if not run port 1337
 //environmental variables is good for sercrets
-const PORT = process.env.PORT || 1337
+//Heroku skapar en env. variabel som heter PORT
+const PORT = process.env.PORT || 1337;
 
+app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res) => {
+  console.log("Web root");
+  res.send("The server is deployed");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}.`)
-})
-
+  console.log(`Server is listening on port ${PORT}.`);
+});
